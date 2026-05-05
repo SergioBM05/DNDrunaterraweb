d7fdf7d8c7015cd6b3eec943aa6c4fba39c1f289
SergioBM05 <serburmej@alu.edu.gva.es>
2026-05-05 19:44:11 +0200

    intento de arreglar vercel

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseKey); 
+// Llamar directamente a import.meta.env dentro de createClient
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+);
